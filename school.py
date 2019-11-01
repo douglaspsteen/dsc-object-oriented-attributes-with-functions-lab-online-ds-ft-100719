2f70240d7204fc01967fff1cae76e7d45c3bf748
Douglas Steen <douglaspsteen@gmail.com>
2019-10-31 20:50:19 -0500

    completed lab

--- school.py
+++ school.py
@@ -1,1 +1,20 @@
 class School:
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+    
+    def add_student(self, studentName, grade):
+        if grade in self.roster:
+            self.roster[grade].append(studentName)
+        else:
+            self.roster[grade] = [studentName]
+    
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    def sort_roster(self):
+        sorted_dict = self.roster
+        for key in sorted_dict:
+            sorted_dict[key].sort()
+        return sorted_dict
+            
